bfeb0cb17a3d6a1531061b9441bfb345346beab7
Rahulbhoyar10 <155287167+Rahulbhoyar10@users.noreply.github.com>
2026-01-13 16:41:15 +0530

    Uncomment pipeline stages in pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,25 +1,26 @@
-// pipeline {
-//     agent any
-//     stages {
-//         stage('git-pull') {
-//             steps {
-//                 echo "pull-sucess"
-//             }
-//         }
-//         stage('Build') {
-//             steps {
-//                 echo "build-sucess"
-//             }
-//         }
-//         stage('Test') {
-//             steps {
-//                 echo "test sucess"
-//             }
-//         }
-//         stage('Deploy') {
-//             steps {
-//                 echo "deploy sucess"
-//             }
-//         }
-//     }
-// }
+pipeline {
+    agent any
+    stages {
+        stage('git-pull') {
+            steps {
+                echo "pull-sucess"
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "build-sucess"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "test sucess"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploy sucess"
+            }
+        }
+    }
+
+}
